c99b077b535547d42339aaabb1a3fb2cc8e4e8f5
sukinoelsuki <sukinoelsuki@gmail.com>
2026-04-20 17:16:15 +0800

    4.20 day work

--- work/kernel/pmm.c
+++ work/kernel/pmm.c
@@ -7,7 +7,13 @@
 #include "spike_interface/spike_utils.h"
 
 // 按照写定的 page_t 结构，将节点的地址转换成 page 的首地址
-#define node_to_page(ptr) ((page_t *)((char *)(ptr) - 8))
+#define NODE_TO_PAGE(ptr) ((page_t *)((char *)(ptr) - 8))
+
+// 各类型地址转化
+#define PFN_TO_PAGE(pfn)  (&pages[(pfn)])
+#define PAGE_TO_PFN(p)    ((uint64)((p) - pages))
+#define PFN_TO_ADDR(pfn)  ((void *)((pfn) * PGSIZE + DRAM_BASE))
+#define ADDR_TO_PFN(addr) (((uint64)(addr) - DRAM_BASE) / PGSIZE)
 
 // _end is defined in kernel/kernel.lds, it marks the ending (virtual) address of PKE kernel
 extern char _end[];
@@ -18,16 +24,16 @@ extern uint64 g_mem_size;
 
 // 链表节点，双向列表实现 O（1） 增删
 // 和原有链表重名了，但是仅是增加了结构，并不会报错
-typedef struct List_Node {
+typedef struct {
   list_node* prev;
   list_node* next;
 } list_node;
 
 // 相关 list_node 双向链表操作
 // 删去 prev 到 next 间的所有节点
-static inline void __list_del(list_node *prev, list_node *next) {
-  next->prev = prev;
-  prev->next = next;
+static inline void __list_del(list_node *node) {
+  node->prev->next = node->next;
+  node->next->prev = node->prev;
 }
 
 // 将一段 new_list 插入到 prev 节点后
@@ -41,12 +47,12 @@ static inline void __list_add(list_node *prev, list_node* new_head, list_node *n
 // 不判断是否为空，保证安全
 static inline list_node* list_pop(list_node *head) {
   list_node *node = head->next;
-  __list_del(node->prev, node->next);
+  __list_del(node);
   return node;
 }
 
 // 整体 Buddy 系统
-typedef struct Buddy_System{
+typedef struct {
   spinlock_t buddy_lock;
   list_node free_area[MAX_ORDER];
 } __attribute__((aligned(64))) buddy_system;
@@ -58,7 +64,7 @@ typedef struct page {
         struct {
             uint32 is_free : 1;  // 空闲标记，1 表示空闲、能够使用。
             uint32 is_head : 1;  // 领头羊标记，仅有块首有标记，用于判断自己是否是“随从”
-            uint32 order   : 5;  // 阶数，表明当前 page 代表的连续页块大小
+            uint32 order   : 5;  // 当前阶数，表明当前 page 代表的连续页块大小，从0阶开始
             uint32 alloc   : 3;  // 归属标记，标识属于的内存系统，Buddy System/ Slab、Slub/Per-CPU Cache/内核静态预留（不可释放）
             uint32 cpu_id  : 6;  // 亲和性标记，属于哪个核（最多支持 64），便于实现跨核释放
             uint32 ref     : 16; // 引用计数，实现 COW
@@ -68,10 +74,18 @@ typedef struct page {
     list_node node;
 } __attribute__((aligned(8))) page_t; // 注意！保证了 status 的原子性，但 node 仍大概率截断。因修改 node 概率小（存疑）
 
+typedef struct percpu_cache {
+  page_t *objs[PCPU_CACHE_SIZE];
+  uint32 count;
+  spinlock_t percpu_lock;
+} __attribute__((aligned(64))) percpu_cache_t;
+
 // 物理页描述符数组指针
 page_t *pages;
 // total num of pa pages
 uint64 nr_pages;
+
+percpu_cache_t pcpu_caches[NCPU];
 
 static uint64 free_mem_start_addr;  //beginning address of free memory
 static uint64 free_mem_end_addr;    //end address of free memory (not included)
@@ -289,10 +303,10 @@ static inline uint64 buddy_system_init(uint64 start_addr, uint64 end_addr) {
     uint64 limit_order = 63 - __builtin_clzll(remaining_pages);
 
     uint64 order = align_order < limit_order ? align_order : limit_order;
-    if (order > MAX_ORDER) order = MAX_ORDER - 1;
+    if (order >= MAX_ORDER) order = MAX_ORDER - 1;
 
-    uint64 PFN = (curr_addr - stop_addr) / PGSIZE;
-    page_t *p = &pages[PFN];
+    uint64 PFN = ADDR_TO_PFN(curr_addr);
+    page_t *p = PFN_TO_PAGE(PFN);
     p->status.alloc = 0;
     p->status.is_head = 1;
     p->status.is_free = 1;
@@ -306,13 +320,12 @@ static inline uint64 buddy_system_init(uint64 start_addr, uint64 end_addr) {
 void buddy_split(uint32 big_order, uint32 target_order) {
   for (int i = big_order; i > target_order; i--) {
     // 将当前阶第一个空闲的 page 取出来
-    ? : sizeof 函数全部直接转化为宏定义的数据？
     list_node *n = list_pop(&main_buddy.free_area[i]);
     page_t *p = node_to_page(n);
     
     // 计算当前页的 buddy，以便实现分割
-    // 分割意味着降级了，所以在这一位上加 1 就是它的 buddy
-    uint64 buddy_pfn = ((uint64)p - (uint64)pages) / sizeof(page_t) + (1ULL << (i - 1));
+    // 分割意味着降级了，所以在下一阶上加 1 就是它的 buddy
+    uint64 buddy_pfn = (uint64)(p - pages) + (1ULL << (i - 1));
     page_t *bud = &pages[buddy_pfn];
     bud->status.alloc = 0;
     bud->status.is_free = 1;
@@ -331,39 +344,100 @@ void buddy_split(uint32 big_order, uint32 target_order) {
 void *buddy_alloc(uint32 order) {
 
   // 操作链表，只能上 Spinlock
-  lock_acquire(main_buddy.buddy_lock.lock);
+  lock_acquire(&main_buddy.buddy_lock.lock);
 
   for(int i = order; i < MAX_ORDER; ++i) {
-    if (main_buddy.free_area[i].next == NULL)
+    if (main_buddy.free_area[i].next == &main_buddy.free_area[i])
       continue;
 
     if (i > order) 
       buddy_split(i, order);
     
     list_node *free_node = list_pop(&main_buddy.free_area[order]);
-    page_t *p = node_to_page(free_node);
-    p->status.alloc = 0;
+    page_t *p = NODE_TO_PAGE(free_node);
+    
+    lock_release(&main_buddy.buddy_lock.lock);
+    // 出厂设置，状态预置为 percpu_cache
     p->status.is_free = 0;
-    // 在 split 时是否已经置 1 了？这可能是一段可用的空间，而接口仅是当前地址
-    p->status.is_head = 1;
-    lock_release(main_buddy.buddy_lock.lock);
+    p->status.alloc = 1;
 
     // 返回物理地址 : pfn * 4096 + DRAM_BASE
-    ? : p - pages 是地址？还是数字？
     uint64 pfn = (uint64)(p - pages);
     return (void *)(pfn * PGSIZE + DRAM_BASE);
   }
 
   // 释放全局锁
-  lock_release(main_buddy.buddy_lock.lock);
+  lock_release(&main_buddy.buddy_lock.lock);
+  panic("buddy_alloc : Out of required size of memory!\n");
   return NULL;
+}
+
+static inline void __buddy_merge_innner(page_t *p) {
+  uint64 pfn = PAGE_TO_PFN(p);
+  uint64 order = p->status.order;
+  
+  p->status.is_free = 0;
+  p->status.is_head = 0;
+  p->status.alloc = 0;
+  
+  while (order < (MAX_ORDER - 1)) {
+    page_t *my_buddy = PFN_TO_PAGE(pfn ^ (1ULL << order));
+
+    if (my_buddy->status.order != order || my_buddy->status.is_free != 1 || my_buddy->status.is_head != 1) 
+      break;
+
+    __list_del(&my_buddy->node);
+    my_buddy->status.is_free = 0;
+    my_buddy->status.is_head = 0;
+
+    pfn &= ~(1ULL << order);
+    p = PFN_TO_PAGE(pfn);
+    order++;
+    p->status.order = order;
+  }
+  p->status.is_free = 1;
+  p->status.is_head = 1;
+  p->status.alloc = 0;
+  __list_add(&main_buddy.free_area[order], &p->node, &p->node);
 }
 
 void buddy_free(void *addr) {
   // 相当耗时的第一个判断，是去掉，在其它地方保证不会放进空指针，
   // 还是说加上 unlikely() 进行剪枝
-  if (unlikely(addr)) return;
-  
+  if (unlikely(!addr)) return;
+  uint64 pfn = ADDR_TO_PFN(addr);
+  lock_acquire(&main_buddy.buddy_lock.lock);
+  __buddy_merge_innner(PFN_TO_PAGE(pfn));
+  lock_release(&main_buddy.buddy_lock.lock);
 }
+
+// 对私有 Cache 集中释放 PERCPU_CACHE_SIZE / 2 个页块（拿一次锁）
+void pfree_batch(percpu_cache_t* cache) {
+  lock_acquire(&main_buddy.buddy_lock.lock);
+
+  for (int i = 0; i < 16; i++) 
+    __buddy_merge_innner(cache->objs[--cache->count]);
+  cache->count -= 16;
+
+  lock_release(&main_buddy.buddy_lock.lock);
+}
+
+// 只分配了大小为 4KB 的块
+void *alloc_page(void) {
+  uint64 cpu_id = read_tp();
+
+  if (pcpu_caches[cpu_id].count <= 2) {
+    // 从 buddy system 中批发
+    lock_acquire(&main_buddy.buddy_lock.lock);
+    for (int i = 0; i < 16; i++) {
+      pcpu_caches[cpu_id].objs[pcpu_caches[cpu_id].count++] = PFN_TO_PAGE(ADDR_TO_PFN(buddy_alloc(0)));
+    }
+    lock_release(&main_buddy.buddy_lock.lock);
+  }
+
+  // 在 percpu cache 中零售
+  return PFN_TO_ADDR(PAGE_TO_PFN(pcpu_caches[cpu_id].objs[--pcpu_caches[cpu_id].count]));
+}
+
 
 #endif
--- work/kernel/pmm.h
+++ work/kernel/pmm.h
@@ -9,10 +9,12 @@
 // 最大的页表数
 #define MAX_PAGE 32768
 
-// 最高物理块的阶，需要在比赛环境下够用
-// 最高阶的物理块大小为 16Mb
-// 支持 Megapage (9 阶 2MB)
-#define MAX_ORDER 12
+// 最高阶数，需要在比赛环境下够用
+// 最高阶的物理块大小为 4Mb，阶数为 10
+// 支持 Megapage (9 阶 2MB)，增加 TLB 命中效率
+#define MAX_ORDER 11
+
+#define PCPU_CACHE_SIZE 32
 
 // Initialize phisical memeory manager
 void pmm_init();
